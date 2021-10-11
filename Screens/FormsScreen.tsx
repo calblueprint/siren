8104eaa9cedc2b375e5fa0262a805192fc23d054
Phoebe Li <liphoebe01@gmail.com>
2021-10-10 17:14:44 -0700

    save phoebe's changes

--- Screens/FormsScreen.tsx
+++ Screens/FormsScreen.tsx
@@ -4,9 +4,9 @@ import { Text, View } from 'react-native';
 import { screenStyles } from './HomeScreen';
 
 export function FormsScreen() {
-    return (
-      <View style={screenStyles.text}>
-        <Text>Fill out your intake form!</Text>
-      </View>
-    );
-  }
+  return (
+    <View style={screenStyles.text}>
+      <Text>Fill out your intake form!</Text>
+    </View>
+  );
+}
