90b963ecfd6182158a2b507904e83035d13d19fb
cnnmon <wangttiffany@gmail.com>
2021-11-05 00:05:49 -0700

    border fix

--- assets/Components.tsx
+++ assets/Components.tsx
@@ -4,14 +4,14 @@ import styled from 'styled-components/native';
 export const ButtonLight = styled.TouchableOpacity`
   text-align: center;
   padding: 0.4em 1em;
-  border: 1px solid black;
+  border-width: 1px;
   border-radius: 5px;
 `;
 
 export const ButtonDark = styled.TouchableOpacity`
   text-align: center;
   padding: 0.4em 1em;
-  border: 1px solid black;
+  border-width: 1px;
   border-radius: 5px;
   background: black;
 `;
