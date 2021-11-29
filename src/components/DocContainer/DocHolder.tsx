1eed994cef9d7ed7d15303378aa4ef0162dbbf89
Shannon Bonet <shannonbonet@berkeley.edu>
2021-11-28 16:12:06 -0800

    eslint changes

--- src/components/DocContainer/DocHolder.tsx
+++ src/components/DocContainer/DocHolder.tsx
@@ -1,52 +1,38 @@
 import React from 'react';
-import { Colors } from '../../../assets/Colors';
-import { TextRegular } from '../../../assets/fonts/Fonts';
 import { AntDesign, Feather, FontAwesome } from '@expo/vector-icons';
+import { TextRegular } from 'assets/fonts/Fonts';
+import { Colors } from 'assets/Colors';
 import { DocSelect, ActionButton } from './styles';
 
-
-
 interface DocProps {
-    title: string;
-    submitted: boolean;
-    onClick?: () => void;
-    
+  title: string;
+  submitted: boolean;
+  // eslint-disable-next-line react/require-default-props
+  onClick?: () => void;
 }
 
+const Upload = <Feather name="upload" />;
 
-const Upload = <Feather name='upload' />
+const Trash = (
+  <FontAwesome name="trash-o" style={{ color: Colors.brandGray }} />
+);
 
-const Trash = <FontAwesome name='trash-o' style = {{ color: Colors.brandGray }}/>
+export const Missing = (
+  <AntDesign name="exclamationcircle" style={{ color: Colors.brandRed }} />
+);
 
-export const Missing = <AntDesign name='exclamationcircle' style={{ color: Colors.brandRed}} />
+export const Submitted = (
+  <AntDesign name="checkcircle" style={{ color: Colors.brandBlue }} />
+);
 
-export const Submitted = <AntDesign name='checkcircle' style={{ color: Colors.brandBlue }} />
-
-
-
-const DocHolder = (props: DocProps) => { 
-    return (
-        <DocSelect onPress={props.onClick}>
-            {props.submitted  ? Submitted : Missing}
-            <TextRegular>{props.title}</TextRegular>
-            <ActionButton>
-                {props.submitted ? Trash : Upload}
-            </ActionButton>
-        </DocSelect>
-        
-
-    );
-}
+const DocHolder = ({ onClick, title, submitted }: DocProps) => {
+  return (
+    <DocSelect onPress={onClick}>
+      {submitted ? Submitted : Missing}
+      <TextRegular>{title}</TextRegular>
+      <ActionButton>{submitted ? Trash : Upload}</ActionButton>
+    </DocSelect>
+  );
+};
 
 export default DocHolder;
-
-
-
-
-
-
-
-
-
-
-
