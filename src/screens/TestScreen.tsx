558f518625cf02b5cf1b8e550da2ee66b762e0f2
Shannon Bonet <shannonbonet@berkeley.edu>
2021-12-03 00:08:06 -0800

    added margin on test screen for iOS emulator accessible

--- src/screens/TestScreen.tsx
+++ src/screens/TestScreen.tsx
@@ -5,7 +5,7 @@ import { ScrollPageContainer, InnerPageContainer } from 'screens/styles';
 const TestScreen = ({ navigation: { goBack } }: any) => {
   return (
     <ScrollPageContainer>
-      <InnerPageContainer>
+      <InnerPageContainer style={{ marginTop: 100 }}>
         <Button title="Go Back" onPress={() => goBack()} />
       </InnerPageContainer>
     </ScrollPageContainer>
