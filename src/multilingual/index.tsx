3d6dbca5c27c3bdd057db43aba0280e189963661
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2022-04-27 23:24:05 -0700

    for shannon

--- src/multilingual/index.tsx
+++ src/multilingual/index.tsx
@@ -1,13 +1,13 @@
-import en from './languages/en.json';
-import es from './languages/es.json';
-import vie from './languages/vie.json';
+import EN from './languages/en.json';
+import ES from './languages/es.json';
+import VIE from './languages/vie.json';
 import { useContext } from 'react';
-import { LanguageContext } from 'context/ContextProvider';
+import { LanguageContext } from '../context/ContextProvider';
 
-export const dictionaryList = { en, es, vie };
+export const dictionaryList = { EN, ES, VIE };
 
 export const languageOptions = {
-  en: 'English',
-  es: 'Spanish',
-  vie: 'Vietnamese',
+  EN: 'English',
+  ES: 'Spanish',
+  VIE: 'Vietnamese',
 };
