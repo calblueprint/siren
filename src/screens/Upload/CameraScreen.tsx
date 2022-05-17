e34db02cb78663dcafbb7e3e781e7cf9450a5f2f
bry-gavino <bry_gavino01@berkeley.edu>
2022-05-17 16:38:41 -0700

    rolled back 1 commit

--- src/screens/Upload/CameraScreen.tsx
+++ src/screens/Upload/CameraScreen.tsx
@@ -20,9 +20,8 @@ import {
   TextSubtitle,
   TextTitle,
 } from 'assets/fonts/Fonts';
-import { setDocument, getClientCaseDocs } from 'database/queries';
+import { setDocument } from 'database/queries';
 import { Text } from 'context/ContextProvider';
-import { Document } from 'types/types';
 import {
   PicturesContainer,
   PageContainer,
@@ -43,23 +42,15 @@ const CameraScreen = ({ navigation, route }: any) => {
   const [modalVisible, setModalVisible] = useState(false);
   const storage = firebase.storage();
   const clientCase = route.params?.clientCase;
-  const caseId = clientCase?.id;
   const clientId = route.params?.clientId;
   const docName = route.params?.name;
 
   useEffect(() => {
-    const loadImages = async (): Promise<void> => {
-      const clientCaseDocs = await getClientCaseDocs(clientId, caseId);
-      setImageUris(
-        clientCaseDocs.filter(doc => doc.type === docName).map(doc => doc.url),
-      );
-    };
-    loadImages();
-    // if (route.params?.uris) {
-    //   // Post updated, do something with `route.params.post`
-    //   // For example, send the post to the server
-    //   setImageUris(route.params.uris);
-    // }
+    if (route.params?.uris) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+      setImageUris(route.params.uris);
+    }
   }, [route.params?.uris]);
 
   useEffect(() => {
@@ -91,8 +82,6 @@ const CameraScreen = ({ navigation, route }: any) => {
   };
 
   const renderCurrentPictures = () => {
-    // setImageUris(allClientDocs.map(doc => doc.url));
-    console.log(imageUris);
     return (
       <PicturesContainer>
         {imageUris.length !== 0
