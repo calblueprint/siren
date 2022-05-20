860abafb25371d9b798ae2b9381a7cda5857f418
bry-gavino <bry_gavino01@berkeley.edu>
2022-05-19 19:03:52 -0700

    implemented redirectable contact info

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useIsFocused } from '@react-navigation/native';
-import { ScrollView } from 'react-native';
+import { Button, Linking, ScrollView } from 'react-native';
 import { PageContainer } from 'screens/styles';
 import { getAllCases, getClient } from 'database/queries';
 import { Case } from 'types/types';
 import firebase from 'firebase';
 import ProgressTracker from 'components/ProgressTracker/ProgressTracker';
-import { TextTitle } from 'assets/fonts/Fonts';
+import { TextBold, TextTitle } from 'assets/fonts/Fonts';
 import { CaseStatus } from 'types/types';
 // eslint-disable-next-line no-restricted-imports
 import { NameContainer } from '../styles';
@@ -37,7 +37,30 @@ const HomeScreen = ({ navigation }: any) => {
       <NameContainer>
         <TextTitle>Hi {name}!</TextTitle>
       </NameContainer>
+
       <ScrollView>
+        <TextBold>Email/Call Bay Area Office: </TextBold>
+        <Button
+          onPress={() =>
+            Linking.openURL('mailto:Info@sirenimmigrantrights.org')
+          }
+          title="Info@sirenimmigrantrights.org"
+        />
+        <Button
+          onPress={() => Linking.openURL(`tel:${4084533003}`)}
+          title="(408) 453-3003"
+        />
+        <TextBold>Email/Call Central Valley Office: </TextBold>
+        <Button
+          onPress={() =>
+            Linking.openURL('mailto:Centralvalley@sirenimmigrantrights.org')
+          }
+          title="Centralvalley@sirenimmigrantrights.org"
+        />
+        <Button
+          onPress={() => Linking.openURL(`tel:${5598400005}`)}
+          title="(559) 840-0005"
+        />
         {cases.length !== 0 ? (
           Object.keys(cases).map((id: any) => (
             <ProgressTracker
